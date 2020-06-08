97a7aaa5714d18af633e080adbe16fdf239eee06
clemEssien <clem.essien@gmail.com>
2020-06-08 08:23:35 -0500

    make api calls to generate sequence and annotations from uniprot db loading ids from file

--- src/main_uniprot.py
+++ src/main_uniprot.py
@@ -15,50 +15,54 @@ from collections import defaultdict
 SEQ_DIR = "../data/uniprot/"
 DATA_DIR = "../data/"
 
+uniprot_ids = []
+with open(SEQ_DIR+"uniprot_ids.txt","r") as file_handle:
+    uniprot_ids = file_handle.read().split('\n')
 
-
-annotations = defaultdict(list)
 URL = 'https://www.uniprot.org/uniprot/'
 
-uniprot_id = 'P45452'
-response = mhr.make_request(URL+uniprot_id+".txt")
-response = response.content.decode('utf-8').strip()
-# print(response)
+for uniprot_id in uniprot_ids:
+    annotations = defaultdict(list)
+    uniprot_id = uniprot_id.strip()
+    print("processing "+uniprot_id)
+    response = mhr.make_request(URL+uniprot_id+".txt")
+    response = response.content.decode('utf-8').strip()
 
-#Zinc, Calcium, Mangnesium, Calcium, Sodium, Iron, Manganese, 
-annotation_dict = defaultdict(list)
-metals = response.split('FT   METAL           ')
+    annotation_dict = defaultdict(list)
+    metals = response.split('FT   METAL           ')
 
+    annotations = metals[1:]
+    annotations = [int(x.split('\nFT')[0]) for x in annotations]
 
-annotations = metals[1:]
-annotations = [int(x.split('\nFT')[0]) for x in annotations]
-print(annotations)
-if len(annotations) < 1:
-    print("no metal site")
-    with open("no_metal_site.txt", "a") as f:
-        f.write(uniprot_id+"\n")
+    if len(annotations) < 1:
+        print("no metal site")
+        with open("no_metal_site.txt", "a") as f:
+            f.write(uniprot_id+"\n")
+    for annotation in annotations:
+                try:
+                        ann = response.split(
+                            '''FT   METAL           '''+str(annotation)+'''
+FT                   /note="'''
+                        )[1].split(' ')[0].strip()
+                        ann = re.compile('[^a-zA-Z]').sub('', ann).replace('FT', "").replace('"','')
+                        if annotation not in annotation_dict[ann]:
+                            annotation_dict[ann].append(annotation)
+                    
+                except:
+                    with open(SEQ_DIR+"error.txt","a") as f:
+                        f.write(uniprot_id+"\n")
+                    
+    sequence = response.strip()
 
-for annotation in annotations:
-            try:
-                    ann = response.split('''FT   METAL           '''+str(annotation)+'''
-FT                   /note="''')[1].split(' ')[0].strip()
-                    ann = re.compile('[^a-zA-Z]').sub('', ann).replace('FT', "")
-                    if annotation not in annotation_dict[ann]:
-                        annotation_dict[ann].append(annotation)
-                   
-            except:
-                a =1
-sequence = response.strip()
+    sequence = sequence[(sequence.rindex(';')+1) : (-2)].strip().replace(' ','')
 
-sequence = sequence[(sequence.rindex(';')+1) : (-2)].strip().replace(' ','')
-
-
-for key, value in annotation_dict.items():
-    print(key)
-    with open(SEQ_DIR+key+".fasta", "a") as file_handle:
-        sequence = sequence.replace("\n",'')
-        sequences = aa.insert_annotations(sequence, value,'#')
-        sequences = textwrap.fill(sequences,60)
-        file_handle.write('>'+uniprot_id +"\n")
-        file_handle.write(sequences+"\n")
-
+    sequences = ''
+    for key, value in annotation_dict.items():
+        print(key)
+        with open(SEQ_DIR+key+".fasta", "a") as file_handle:
+            sequence = sequence.replace("\n",'')
+            sequences = aa.insert_annotations(sequence, value,'#')
+            sequences = textwrap.fill(sequences,60)
+            if len(sequences) > 0:
+                file_handle.write('>'+uniprot_id +"\n")
+                file_handle.write(sequences+"\n")
